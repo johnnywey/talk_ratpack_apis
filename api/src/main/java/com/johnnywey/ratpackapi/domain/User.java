a03364b26bf810bf9814b49dc2b282ea8fd68c4d
Johnny Wey <johnnywey@gmail.com>
2014-12-03 16:12:21 -0700

    Add custom attribution for User

--- api/src/main/java/com/johnnywey/ratpackapi/domain/User.java
+++ api/src/main/java/com/johnnywey/ratpackapi/domain/User.java
@@ -1,6 +1,9 @@
 package com.johnnywey.ratpackapi.domain;
 
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonValue;
 import de.caluga.morphium.annotations.Entity;
 import de.caluga.morphium.annotations.Id;
 import de.caluga.morphium.annotations.Index;
@@ -17,12 +20,14 @@ public class User {
     private final static String DEFAULT_AVATAR_URL = "/public/images/no-avatar.png";
 
     @Id
+    @JsonIgnore
     private ObjectId objectId;
     private String firstName;
     private String lastName;
     private String emailAddress;
     @Index
     private String username;
+    @JsonIgnore
     private String password;
     private List<UserRole> roles = new ArrayList<>();
     private String imageUrl = DEFAULT_AVATAR_URL;
@@ -107,5 +112,13 @@ public class User {
 
     public void setCreateTime(Date createTime) {
         this.createTime = createTime;
+    }
+
+    /**
+     * Include in the JSON output.
+     */
+    @JsonInclude
+    public String getId() {
+        return this.objectId.toString();
     }
 }
